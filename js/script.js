a9deadb414f3def2f25c97bf69acb7bf0a3d83da
oscar leon <oscar.leon.delmar@gmail.com>
2019-07-15 16:35:09 -0500

    three levels added

--- js/script.js
+++ js/script.js
@@ -49,7 +49,7 @@ goal.onload = function () {
 let cloud = new Image();
 cloud.src = "images/cloud1.png";
 cloud.onload = function () {
-    ctx.drawImage(cloud, 110, 510, 90, 90);
+    ctx.drawImage(cloud, 10, 510, 90, 90);
     ctx.drawImage(cloud, 210, 510, 90, 90);
     ctx.drawImage(cloud, 310, 510, 90, 90);
     // 
@@ -62,13 +62,13 @@ cloud.onload = function () {
     ctx.drawImage(cloud, 310, 110, 90, 90);
     ctx.drawImage(cloud, 510, 110, 90, 90);
 }
-let cloud1 = new Image();
-cloud1.src = "images/huricane.png";
-cloud1.onload = function () {
-    ctx.drawImage(cloud1, 220, 420, 50, 80);
-    ctx.drawImage(cloud1, 20, 20, 50, 80);
-    ctx.drawImage(cloud1, 620, 120, 50, 80);
-    ctx.drawImage(cloud1, 320, 220, 50, 80);
+let huricane = new Image();
+huricane.src = "images/huricane.png";
+huricane.onload = function () {
+    ctx.drawImage(huricane, 220, 420, 50, 80);
+    ctx.drawImage(huricane, 20, 20, 50, 80);
+    ctx.drawImage(huricane, 620, 120, 50, 80);
+    ctx.drawImage(huricane, 320, 220, 50, 80);
 }
 let coin = new Image();
 coin.src = "images/static-coin.png";
@@ -86,31 +86,77 @@ coin.onload = function () {
 // coinImage.onload = function(){
 //     ctx.drawImage(coinImage, 90,130,50,60,10,10,50,60)
 // }
-let boat = new Image();
-boat.src = "images/fancy-boat.png";
-boat.onload = function () {
+// let boat = new Image();
+// boat.src = "images/fancy-boat.png";
+// boat.onload = function () {
     
-    ctx.drawImage(boat, 20, 520, 50, 80);
-}
-function boatMove(e){
-    switch(e.keyCode){
+//     ctx.drawImage(boat, 110, 520, 80, 80);
+// }
+class Boat {
+    constructor(){
+        this.x = 110;
+        this.y = 520;
+        this.width = 80;
+        this.height = 80;
+        this.img = "images/fancy-boat.png";
+    }
+    drawBoat(){
+        let boatImage = new Image();
+        boatImage.src = this.img;
+        boatImage.onload = () => {
+           console.log("----", this);
+          ctx.drawImage(boatImage, this.x, this.y, this.width, this.height);
+        };
+      }
+      moveBoat(num){
+        ctx.clearRect(this.x, this.y, this.width, this.height);
+        switch(num){
         case 38:
-            boat.y -= 50;
-            console.log("you moved a bit");
+            if(this.y > 10){
+            this.y -= 50;
+            }
         break;
         case 40:
-            boat.y += 50;
-            console.log("you moved a bit");
+            if(this.y < 600){
+            this.y += 50;
+            }
         break;
-        case 37:
-            boat.x -= 50;
-            console.log("you moved a bit");
-        break;
-        case 39:
-            boat.x += 50;
-            console.log("you moved a bit");
-        break;
-    }
+          case 37:
+          if(this.x > 20){
+            this.x -= 10;
+          }
+            break;
+          case 39:
+          if (this.x < 430 ){
+            this.x += 10;
+          }
+            break;
+          default:
+            console.logA("What are you doping?");
+        }
+        // this.drawBoat();
 }
-};
+
+
+// function boatMove(e){
+//     switch(e.keyCode){
+//         case 38:
+//             boat.y -= 50;
+//             console.log("you moved a bit");
+//         break;
+//         case 40:
+//             boat.y += 50;
+//             console.log("you moved a bit");
+//         break;
+//         case 37:
+//             boat.x -= 50;
+//             console.log("you moved a bit");
+//         break;
+//         case 39:
+//             boat.x += 50;
+//             console.log("you moved a bit");
+//         break;
+//     }
+// }
+// };
 ///////////////////////////////////////////////////////////////////////////////////
